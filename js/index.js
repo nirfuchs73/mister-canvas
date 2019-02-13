7a928b33ece1456560dd2b1894d8f65eb5d1b91e
nirfuchs\fuchs <nirfuchs@hotmail.com>
2019-02-13 16:50:32 +0200

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,124 @@
+'use strict';
+console.log('Mister canvas');
+
+var gCanvas;
+var gCtx;
+var gColor;
+var gClicks;
+
+function init() {
+    gCanvas = document.querySelector('#my-canvas');
+    gCtx = gCanvas.getContext('2d');
+    gClicks = [];
+    // gCanvas.addEventListener('mousedown', onCanvasMouseDown, false);
+    // console.log(gCtx);
+}
+
+function colorChanged() {
+    gColor = document.getElementById('color').value;
+    // console.log(gColor);
+    // drawCircle();
+    // drawTriangle();
+}
+
+function onCanvasMouseDown(ev) {
+    var elsShape = document.querySelector('.shape');
+    // console.log(elsShape.value);
+    var shape = elsShape.value;
+    var coord = { x: ev.offsetX, y: ev.offsetY };
+    gClicks.push(coord);
+    if (shape === 'line') {
+        if (gClicks.length === 2) { //draw line
+            drawLine(gClicks);
+            emptyArry(gClicks);
+        }
+    }
+    if (shape === 'rectangle') { //draw rectangle
+        if (gClicks.length === 4) {
+            drawRectangle(gClicks);
+            emptyArry(gClicks);
+        }
+    } else if (shape === 'circle') { // draw circle
+        if (gClicks.length === 2) {
+            drawCircle(gClicks);
+            emptyArry(gClicks);
+        }
+    } else { //draw triangle
+        if (gClicks.length === 3) {
+            drawTriangle(gClicks);
+            emptyArry(gClicks);
+        }
+    }
+
+    // console.log(ev.x);
+    // console.log(ev.y);
+}
+
+function drawLine(clicks) {
+    if (clicks.length === 2) {
+        gCtx.beginPath();
+        gCtx.moveTo(clicks[0].x, clicks[0].y);
+        gCtx.lineTo(clicks[1].x, clicks[1].y);
+        gCtx.strokeStyle = gColor;
+        gCtx.closePath();
+        gCtx.stroke();
+    }
+}
+
+function drawRectangle(clicks) {
+    if (clicks.length === 4) {
+        gCtx.beginPath();
+        gCtx.moveTo(clicks[0].x, clicks[0].y);
+        gCtx.lineTo(clicks[1].x, clicks[1].y);
+        gCtx.lineTo(clicks[2].x, clicks[2].y);
+        gCtx.lineTo(clicks[3].x, clicks[3].y);
+        gCtx.strokeStyle = gColor;
+        gCtx.closePath();
+        gCtx.stroke();
+    }
+}
+
+function drawCircle(clicks) {
+    console.log('drawCircle');
+    gCtx.beginPath();
+    gCtx.moveTo(clicks[0].x, clicks[0].y);
+    gCtx.arc(clicks[0].x, clicks[0].y, 50, 0, 2 * Math.PI);
+    // gCtx.arc(100, 300, 50, 0, 2 * Math.PI);
+    // gCtx.arcTo(clicks[0].x, clicks[0].y, clicks[1].x, clicks[1].y, 100);
+    gCtx.strokeStyle = gColor;
+    gCtx.closePath();
+    gCtx.stroke();
+    // gCtx.fill();
+}
+
+function drawTriangle(clicks) {
+    if (clicks.length === 3) {
+        gCtx.beginPath();
+        gCtx.moveTo(clicks[0].x, clicks[0].y);
+        gCtx.lineTo(clicks[1].x, clicks[1].y);
+        gCtx.lineTo(clicks[2].x, clicks[2].y);
+        gCtx.strokeStyle = gColor;
+        gCtx.closePath();
+        gCtx.stroke();
+    }
+}
+
+function onSave() {
+
+}
+
+function onClear() {
+    var doClear = confirm('Are you sure?');
+    if (doClear) {
+        gCtx.clearRect(0, 0, gCanvas.width, gCanvas.height);
+        emptyArry(gClicks);
+    }
+}
+
+function onAbout() {
+    alert('Mister canvas 1.0');
+}
+
+function emptyArry(array) {
+    array.splice(0, array.length);
+}
